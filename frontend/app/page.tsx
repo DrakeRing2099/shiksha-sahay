d63fe4cb988b21b8502c366673b3b7d10427e38c
DrakeRing2099 <sarthakpathak567@gmail.com>
2026-01-11 22:27:39 +0530

    starting structure

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <button className="rounded bg-black px-4 py-2 text-white">
+        Start Session
+      </button>
+    </main>
+  );
+}
